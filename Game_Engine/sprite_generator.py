7796881b7f011867014f10d06c5433478af1fa63
SuperTapood <yoav.o252@gmail.com>
2020-06-23 16:27:43 +0300

    updated sprite_generator

--- Game_Engine/sprite_generator.py
+++ Game_Engine/sprite_generator.py
@@ -1,17 +1,29 @@
-from .sprite import Sprite
-
-
 class Sprite_Generator:
-	def __init__(self, img, x, y , screen, custom_sprite=Sprite):
-		self.dummy = custom_sprite(img, x, y, screen, dummy=True)
+	def __init__(self, mask, screen, kids):
+		self.screen = screen
+		self.mask = mask
+		self.screen.add_generator(self)
 		self.children = []
-		screen.add_generator(self)
-		return
-
-	def update_children(self):
-		for child in self.children:
-			child.update()
+		self.add_children(kids)
 		return
 
 	def update(self):
+		for child in self.children:
+			child.update_sprite()
+		self.check_children()
+		return
+
+	def add_children(self, amount):
+		for _ in range(amount):
+			self.add_child()
+		return
+
+	def add_child(self):
+		self.children.append(self.mask(self.screen))
+		return
+
+	def check_children(self):
 		pass
+
+	def remove_child(self, child):
+		self.children.remove(child)
