871f120e8aded9eb52ebf433b651b0cfe043b635
SuperTapood <yoav.o252@gmail.com>
2020-06-24 17:02:31 +0300

    updated test 4

--- engineTest4.py
+++ engineTest4.py
@@ -7,7 +7,7 @@ import numpy as np
 class Screen(Screen):
 	@override(Screen)
 	def event_handler(self):
-		global x_box, move_factor, scr
+		global x_box, move_factor
 		for event in pygame.event.get():
 			self.__quit_handle(event)
 		keys = pygame.key.get_pressed()
@@ -16,7 +16,7 @@ class Screen(Screen):
 			x_box = max(x_box, 0)
 		elif keys[pygame.K_RIGHT]:
 			x_box += move_factor
-			x_box = min(x_box, scr.X - 50)
+			x_box = min(x_box, self.X - 50)
 		self.update()
 		return
 	pass
@@ -24,7 +24,6 @@ class Screen(Screen):
 class Meteors(Sprite_Generator):
 	@override(Sprite_Generator)
 	def check_children(self):
-		print(len(self.children))
 		for child in self.children:
 			x = child.x
 			y = child.y
@@ -33,6 +32,7 @@ class Meteors(Sprite_Generator):
 					self.remove_child(child)
 					self.add_child()
 		return
+	pass
 
 
 
@@ -40,16 +40,17 @@ class Meteors(Sprite_Generator):
 class Meteor(Sprite):
 	def __init__(self, scr, dummy=False):
 		super().__init__(m_img, 0, 0, scr, dummy=dummy)
-		self.x = np.random.uniform(0, scr.MIDDLEX)
-		self.y = np.random.uniform(0, -200)
-		self.speed_x = np.random.uniform(0, 0.2)
-		self.speed_y = np.random.uniform(0, 0.2)
+		self.x = np.random.uniform(0, scr.X)
+		self.y = 0
+		self.speed_x = np.random.uniform(-0.2, 0.2)
+		self.speed_y = np.random.uniform(0.1, 0.2)
 		return
 
 	def update(self):
 		self.x += self.speed_x
 		self.y += self.speed_y
 		return
+	pass
 
 
 
@@ -57,8 +58,9 @@ if __name__ == "__main__":
 	scr = Screen(x=600, y=500, caption="Space Invaders")
 	x_box = scr.MIDDLEX
 	m_img = scr.load_image("folder\\bird1.png")
-	meteors = Meteors(Meteor, scr, kids=8)
+	meteors = Meteors(Meteor, scr, kids=20)
 	move_factor = 0.5
 	while True:
-		scr.event_handler()
 		player = scr.add_button(x_box, 425, 50, 50, GREEN)
+		scr.event_handler()
+		
